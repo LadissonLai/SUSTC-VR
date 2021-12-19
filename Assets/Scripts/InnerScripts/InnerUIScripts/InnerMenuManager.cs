5e9c29aca2c2439f27c62c928e399d8a235e66ce
SmireG <402809775@qq.com>
2021-12-19 13:33:28 +0800

    setting and exit

--- Assets/Scripts/InnerScripts/InnerUIScripts/InnerMenuManager.cs
+++ Assets/Scripts/InnerScripts/InnerUIScripts/InnerMenuManager.cs
@@ -5,10 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class InnerMenuManager : MonoBehaviour
 {
-    public void onClickSetting() {
-
+    Stack<Transform> st = new Stack<Transform>();
+    void Start()
+    {
+        st.Clear();
+        st.Push(transform.Find("MenuCanvas"));
+        showPanel(st.Peek());
     }
-    public void OnClickReturnMain() {
+    private void showPanel(Transform panel)
+    {
+        panel.GetComponent<CanvasGroup>().alpha = 1;
+        panel.GetComponent<CanvasGroup>().interactable = true;
+        panel.GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+    private void hidePanel(Transform panel)
+    {
+        panel.GetComponent<CanvasGroup>().alpha = 0;
+        panel.GetComponent<CanvasGroup>().interactable = false;
+        panel.GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+    private void switchPanel(string canvasName)        
+    {
+        var panel = transform.Find(canvasName);
+        if (panel) {
+            hidePanel(st.Peek());
+            st.Push(panel);
+            showPanel(st.Peek());
+        }
+    } 
+    public void onClickSetting() {
+        switchPanel("SettingCanvas");
+    }
+    public void onClickReturnMain() {
         SceneManager.LoadScene(0);
+    }
+    public void onClickReturn() {
+        if(st.Count > 0)
+        {
+            hidePanel(st.Pop());
+        }
+        if(st.Count > 0)
+        {
+            showPanel(st.Peek());
+        }
     }
 }
