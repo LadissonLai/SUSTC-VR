97cb73f92c6af1f27c586452105aeb8b9268b4fa
SmireG <402809775@qq.com>
2022-02-19 18:35:34 +0800

    screen

---
+++ Assets/Scripts/MainScreen/OperationStepsScript.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LitJson;
+using System.IO;
+
+public class OperationStep {
+    public string name { get; set; }
+    public bool done { get; set; }
+}
+public class OperationSinglePage {
+    public string title { get; set; }
+    public string description { get; set;}
+    public List<OperationStep> steps { get; set; }
+}
+public class OperationPages {
+    public List<OperationSinglePage> pages { get; set; }
+}
+public class OperationStepsScript : MonoBehaviour
+{
+    public GameObject UpBtn;
+    public GameObject DownBtn;
+    public GameObject Step;
+    public GameObject Content;
+    private OperationPages obj;
+    private List<GameObject> steps;
+
+    // isOperate表示给的UI设计图的右图。默认为false，左图
+    public bool isOperate = false;
+    
+    private int curPage = 0;
+
+    private int doneCount = 0;
+
+    private int maxPageIndex;
+
+    private Color DarkGray = Color.grey;
+    private Color Black = Color.black;
+    // Start is called before the first frame update
+    void Start() {
+        obj = ReadJson("/Data/OperationStepsData.json");
+        maxPageIndex = obj.pages.Count;
+        steps = new List<GameObject>();
+        if(maxPageIndex > 0) {
+            loadScreen(0);
+        }
+        checkInteractable();
+    }
+
+    private OperationPages ReadJson(string str) {
+        StreamReader StreamReader = new StreamReader(Application.dataPath + str);
+        JsonReader js = new JsonReader(StreamReader);
+        return JsonMapper.ToObject<OperationPages>(js);
+    }
+
+    private void checkStepState(GameObject step, bool done, bool  lastDone) {
+        var imgColor = step.GetComponentInChildren<Image>().color;
+        if(isOperate) {
+            if(done) {
+                doneCount++;
+                // 颜色设置为灰色，显示√
+                step.GetComponentInChildren<Text>().color = DarkGray;
+                Debug.Log(step.GetComponentInChildren<Text>().color);
+                step.GetComponentInChildren<Image>().color = new Color(imgColor.r, imgColor.g, imgColor.b,255);
+            } else {
+                // 颜色设置为黑色，隐藏√，若上一个做完了，则当前step字体变大
+                step.GetComponentInChildren<Text>().color = Black;
+                if(lastDone) {
+                   step.GetComponentInChildren<Text>().fontSize = (int)((double)step.GetComponentInChildren<Text>().fontSize * 1.5);
+                }
+                step.GetComponentInChildren<Image>().color = new Color(imgColor.r, imgColor.g, imgColor.b,0);
+            }
+        } else {
+            // 颜色设置为黑色，隐藏√
+            step.GetComponentInChildren<Text>().color = Black;
+            step.GetComponentInChildren<Image>().color = new Color(imgColor.r, imgColor.g, imgColor.b,0);
+        }
+
+    }
+    void checkInteractable() {
+        if(curPage > 0) {
+            UpBtn.GetComponent<Button>().interactable = true;
+        } else {
+            UpBtn.GetComponent<Button>().interactable = false;
+        }
+        if(curPage < maxPageIndex - 1) {
+            DownBtn.GetComponent<Button>().interactable = true;
+        } else {
+            DownBtn.GetComponent<Button>().interactable = false;
+        }
+    }
+    public void onClickPageUp() {
+        curPage = (curPage + 1 + maxPageIndex) % maxPageIndex;
+        loadScreen(curPage);
+        checkInteractable();
+    }
+    public void onClickPageDown() {
+        curPage = (curPage - 1 + maxPageIndex) % maxPageIndex;
+        loadScreen(curPage);
+        checkInteractable();
+    }
+
+    // 暴露出的接口
+
+    // 加载指定页面
+    public void loadScreen(int index) {
+        // initialize
+        foreach(var item in steps) {
+            Destroy(item);
+        }
+        doneCount = 0;
+
+        // load new steps
+        for (int i = 0; i < obj.pages[index].steps.Count; i++) {
+            GameObject tmpStep = Instantiate(Step, Content.transform) as GameObject;
+            tmpStep.GetComponentInChildren<Text>().text = (i+1).ToString() + ". " + obj.pages[index].steps[i].name;
+            Debug.Log(obj.pages[index].steps[i].name);
+            bool lastDone;
+            if(i == 0){
+                lastDone = false;
+            } else {
+                lastDone = obj.pages[index].steps[i - 1].done;
+            }
+            checkStepState(tmpStep, obj.pages[index].steps[i].done, lastDone);
+            tmpStep.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -30 - (50) * i);
+            steps.Add(tmpStep);
+        }
+
+        Content.GetComponent<RectTransform>().sizeDelta = new Vector2(Content.GetComponent<RectTransform>().sizeDelta.x, 50 * obj.pages[index].steps.Count);
+        foreach(var item in GetComponentsInChildren<Text>()) {
+            if(item.name == "Title") {
+                item.text = obj.pages[index].title;
+            }
+            if(item.name == "Description") {
+                item.text = obj.pages[index].description;
+            }
+            if(item.name == "DoneCount") {
+                item.text = doneCount.ToString() + "/" + obj.pages[index].steps.Count.ToString();
+            }
+        }
+
+        // 若isOperate，隐藏上下按钮
+        if(isOperate) {
+            UpBtn.SetActive(false);
+            DownBtn.SetActive(false);
+        } else {
+            UpBtn.SetActive(true);
+            DownBtn.SetActive(true);
+        }
+    }
+    // 完成第几个step
+    public void doneStep(int index) {
+        obj.pages[curPage].steps[index].done = true;
+        loadScreen(curPage);
+    }
+
+}
---
+++ Assets/Scripts/MainScreen/RecordTableScript.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LitJson;
+using System.IO;
+
+public class RecordError {
+    public int index { get; set; }
+    public string description { get; set; }
+    public double punishment { get; set; }
+}
+public class RecordSingleSection {
+    public string title { get; set; }
+    public int totalScore { get; set; }
+    public List<string> records { get; set; }
+    public List<RecordError> errors { get; set; }
+}
+public class RecordSinglePage {
+    public string title { get; set; }
+    public List<RecordSingleSection> sections { get; set; }
+}
+public class RecordPages {
+    public List<RecordSinglePage> pages { get; set; }
+}
+
+public class RecordTableScript : MonoBehaviour
+{
+
+    public GameObject Content;
+    public GameObject PageTitle;
+    public GameObject RecordTitle;
+    public GameObject RecordContent;
+    public GameObject RecordError;
+    public GameObject ScoreContent;
+    private RecordPages obj;
+    private int maxPageIndex;
+    private List<GameObject> records;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = ReadJson("/Data/RecordStepsData.json");
+        maxPageIndex = obj.pages.Count;
+        records = new List<GameObject>();
+        loadScreen(0);
+    }
+
+    private RecordPages ReadJson(string str) {
+        StreamReader StreamReader = new StreamReader(Application.dataPath + str);
+        JsonReader js = new JsonReader(StreamReader);
+        return JsonMapper.ToObject<RecordPages>(js);
+    }
+
+    // 暴露的接口
+
+    // ！！请在load前先写好error信息！
+    public void insertError(int pageIndex, int sectionIndex, int stepIndex, string description, double punishment) {
+        RecordError err = new RecordError();
+        err.index = stepIndex;
+        err.description = description;
+        err.punishment = punishment;
+        obj.pages[pageIndex].sections[sectionIndex].errors.Add(err);
+    }
+    public void loadScreen(int index) {
+        // initialize
+        foreach(var item in records) {
+            Destroy(item);
+        }
+
+        PageTitle.GetComponent<Text>().text = obj.pages[index].title;
+        // 用于计数现在多少条，起始一条为offset
+        int recordCount = 1;
+        double score = 100;
+        var sections = obj.pages[index].sections;
+        foreach(var section in sections) {
+            // section的标题
+            GameObject tmpTitle = Instantiate(RecordTitle, Content.transform) as GameObject;
+            foreach(var item in tmpTitle.GetComponentsInChildren<Text>()) {
+                if(item.name == "Title") {
+                    item.text = section.title;
+                }
+                if(item.name == "TotalScore") {
+                    item.text = "(" + section.totalScore + "分)";
+                }  
+            }
+            foreach(Transform t in tmpTitle.GetComponentsInChildren<Transform>()) {
+                if(t.name == "TotalScore") {
+                    t.GetComponent<RectTransform>().anchoredPosition = new Vector2(70 + section.title.Length * 18, t.GetComponent<RectTransform>().anchoredPosition.y);
+                }  
+            }
+            tmpTitle.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -30 * recordCount);
+            records.Add(tmpTitle);
+            recordCount++;
+
+            // section的内容
+            foreach(var content in section.records) {
+                GameObject tmpContent = Instantiate(RecordContent, Content.transform) as GameObject;
+                tmpContent.GetComponentInChildren<Text>().text = content;
+                tmpContent.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -30 * recordCount);
+                records.Add(tmpContent);
+                recordCount++;
+            }
+
+            // section的error
+            foreach(var err in section.errors) {
+                GameObject tmpError = Instantiate(RecordError, Content.transform) as GameObject;
+                foreach(var item in tmpError.GetComponentsInChildren<Text>()) {
+                    if(item.name == "Title") {
+                        item.text = err.index.ToString() + ". " + err.description;
+                    }
+                    if(item.name == "Score") {
+                        item.text = err.punishment.ToString();
+                        score += err.punishment;
+                    }  
+                }
+                tmpError.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -30 * recordCount);
+                records.Add(tmpError);
+                recordCount++;
+            }
+        }
+        // 总成绩
+        ScoreContent.GetComponent<Text>().text = score.ToString();
+    }
+}
